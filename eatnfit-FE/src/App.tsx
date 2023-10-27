e0d73132dffed09a3adc8f12fa14da4181e2a15d
최정윤 <ej921004@naver.com>
2023-10-28 03:18:47 +0900

    Feat : add signupPage's routing

--- eatnfit-FE/src/App.tsx
+++ eatnfit-FE/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { LandingPage } from "./pages";
+import { LandingPage, SignUpPage } from "./pages";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ eatnfit-FE/src/pages/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,8 @@
+export default function SignUpPage() {
+  return (
+    <div>
+      <h2>회원가입</h2>
+      <form action=""></form>
+    </div>
+  );
+}
