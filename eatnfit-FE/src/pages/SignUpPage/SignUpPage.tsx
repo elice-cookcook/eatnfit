593348271392e1720d8fa5b78d367c9f860859be
최정윤 <ej921004@naver.com>
2023-10-28 03:24:42 +0900

    Chore : set states at SignupPage

--- eatnfit-FE/src/pages/SignUpPage/SignUpPage.tsx
+++ eatnfit-FE/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export default function SignUpPage() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [height, setHeight] = useState<number>();
+  const [weight, setWeight] = useState<number>();
+  const [goalWeight, setGoalWeight] = useState<number>();
   return (
     <div>
       <h2>회원가입</h2>
