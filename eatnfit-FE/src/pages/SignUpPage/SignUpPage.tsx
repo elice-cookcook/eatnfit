ca7a5b88cb78ae6a9ddc8d8d0ab3200c7c1fa109
최정윤 <ej921004@naver.com>
2023-10-29 23:00:39 +0900

    Chore : apply authInput component at signUpPage

--- eatnfit-FE/src/pages/SignUpPage/SignUpPage.tsx
+++ eatnfit-FE/src/pages/SignUpPage/SignUpPage.tsx
@@ -8,12 +8,11 @@ import {
   TbHeartFilled,
   TbHeart,
 } from "react-icons/tb";
-import styled from "styled-components";
-const InputStatus = {
-  NORMAL: "normal",
-  ERROR: "error",
-  SUCCESS: "success",
-};
+import InputStatus from "../../utils/inputStatus";
+import { AuthInput } from "../../components/AuthInput";
+import { AuthSubmitButton } from "../../components/AuthSubmitButton";
+import { FeildWrapper, Form, Message, Title, Wrapper } from "./styles";
+
 export default function SignUpPage() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -46,15 +45,13 @@ export default function SignUpPage() {
       <Title>회원가입</Title>
       <Form action="">
         <FeildWrapper $message={true}>
-          <InputWrapper status={nameInputStatus}>
-            <TbUser size={"20px"}></TbUser>
-            <Input
-              type="text"
-              value={name}
-              placeholder="이름"
-              onChange={(e) => setName(e.target.value)}
-            ></Input>
-          </InputWrapper>
+          <AuthInput
+            icon={<TbUser size={"20px"} />}
+            status={nameInputStatus}
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            placeholder="이름"
+          />
           <Message>
             {nameInputStatus === InputStatus.ERROR
               ? "이름을 정확히 입력하세요"
@@ -62,15 +59,14 @@ export default function SignUpPage() {
           </Message>
         </FeildWrapper>
         <FeildWrapper $message={true}>
-          <InputWrapper status={emailInputStatus}>
-            <TbMail size={"20px"}></TbMail>
-            <Input
-              type="email"
-              value={email}
-              placeholder="아이디(이메일)"
-              onChange={(e) => setEmail(e.target.value)}
-            ></Input>
-          </InputWrapper>
+          <AuthInput
+            status={emailInputStatus}
+            icon={<TbMail size={"20px"} />}
+            type="email"
+            value={email}
+            placeholder="아이디(이메일)"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Message>
             {emailInputStatus === InputStatus.ERROR
               ? "아이디는 이메일 형식으로 입력해주세요."
@@ -78,15 +74,15 @@ export default function SignUpPage() {
           </Message>
         </FeildWrapper>
         <FeildWrapper $message={true}>
-          <InputWrapper status={passwordInputStatus}>
-            <TbLock size={"20px"}></TbLock>
-            <Input
-              type="password"
-              value={password}
-              placeholder="비밀번호"
-              onChange={(e) => setPassword(e.target.value)}
-            ></Input>
-          </InputWrapper>
+          <AuthInput
+            status={passwordInputStatus}
+            icon={<TbLock size={"20px"} />}
+            type="password"
+            value={password}
+            placeholder="비밀번호"
+            onChange={(e) => setPassword(e.target.value)}
+            isPassword
+          />
           <Message>
             {passwordInputStatus === InputStatus.ERROR
               ? "비밀번호는 8자 이상이어야 합니다. "
@@ -94,15 +90,15 @@ export default function SignUpPage() {
           </Message>
         </FeildWrapper>
         <FeildWrapper $message={true}>
-          <InputWrapper status={confirmPasswordInputStatus}>
-            <TbLockCheck size={"20px"}></TbLockCheck>
-            <Input
-              type="password"
-              value={confirmPassword}
-              placeholder="비밀번호 확인"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            ></Input>
-          </InputWrapper>
+          <AuthInput
+            status={confirmPasswordInputStatus}
+            icon={<TbLockCheck size={"20px"} />}
+            type="password"
+            value={confirmPassword}
+            placeholder="비밀번호 확인"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            isPassword
+          />
           <Message>
             {confirmPasswordInputStatus === InputStatus.ERROR
               ? "비밀번호가 일치하지 않습니다."
@@ -110,114 +106,44 @@ export default function SignUpPage() {
           </Message>
         </FeildWrapper>
         <FeildWrapper>
-          <InputWrapper status={heightInputStatus}>
-            <TbMan size={"20px"}></TbMan>
-            <Input
-              type="number"
-              value={height}
-              placeholder="신장(키)"
-              onChange={(e) => setHeight(parseInt(e.target.value))}
-            ></Input>
-            <span>cm</span>
-          </InputWrapper>
+          <AuthInput
+            status={heightInputStatus}
+            icon={<TbMan size={"20px"} />}
+            type="number"
+            value={height}
+            placeholder="신장(키)"
+            onChange={(e) => setHeight(parseInt(e.target.value))}
+            unit={"cm"}
+          />
         </FeildWrapper>
         <FeildWrapper>
-          <InputWrapper status={weightInputStatus}>
-            <TbHeart size={"20px"}></TbHeart>
-            <Input
-              type="number"
-              value={height}
-              placeholder="체중(몸무게)"
-              onChange={(e) => setWeight(parseInt(e.target.value))}
-            ></Input>
-            <span>kg</span>
-          </InputWrapper>
+          <AuthInput
+            status={weightInputStatus}
+            icon={<TbHeart size={"20px"} />}
+            type="number"
+            value={weight}
+            placeholder="체중(몸무게)"
+            onChange={(e) => setWeight(parseInt(e.target.value))}
+            unit={"kg"}
+          />
         </FeildWrapper>
         <FeildWrapper>
-          <InputWrapper status={goalWeightInputStatus}>
-            <TbHeartFilled size={"20px"}></TbHeartFilled>
-            <Input
-              type="number"
-              value={goalWeight}
-              placeholder="목표 체중(몸무게)"
-              onChange={(e) => setGoalWeight(parseInt(e.target.value))}
-            ></Input>
-            <span>kg</span>
-          </InputWrapper>
+          <AuthInput
+            status={goalWeightInputStatus}
+            type="number"
+            value={goalWeight}
+            placeholder="목표 체중(몸무게)"
+            onChange={(e) => setGoalWeight(parseInt(e.target.value))}
+            icon={<TbHeartFilled size={"20px"} />}
+            unit={"kg"}
+          />
         </FeildWrapper>
       </Form>
-      <SubmitButton>회원가입</SubmitButton>
+
+      <AuthSubmitButton
+        text={"회원가입"}
+        onClick={() => alert("환영합니다.")}
+      />
     </Wrapper>
   );
 }
-
-const Wrapper = styled.div`
-  height: 90vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const Title = styled.h1`
-  text-align: center;
-  color: #00a9ff;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
-
-const FeildWrapper = styled.div<{ $message?: boolean }>`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  justify-items: center;
-  align-items: center;
-  margin-bottom: ${({ $message }) => ($message ? 0 : "21px")};
-`;
-
-const InputWrapper = styled.div<{ status?: string }>`
-  display: flex;
-  align-items: center;
-  padding: 10px;
-  width: 50%;
-  border: 1px solid gray;
-  border-bottom: solid
-    ${({ status }) =>
-      status === InputStatus.ERROR
-        ? "2px tomato"
-        : status === InputStatus.SUCCESS
-        ? "2px green"
-        : "1px gray"};
-  border-radius: 3px;
-`;
-
-const Input = styled.input`
-  border: none;
-  width: 70%;
-  font-size: 14px;
-  background-color: transparent;
-  margin: 0 10px;
-  padding: 5px;
-`;
-
-const Message = styled.p`
-  font-weight: bold;
-  font-size: 12px;
-  color: tomato;
-  height: 15px;
-  padding: 0;
-  margin: 3px 0;
-`;
-
-const SubmitButton = styled.button`
-  background-color: #89cff3;
-  border: none;
-  width: 50%;
-  align-self: center;
-  margin: 20px;
-  font-size: 16px;
-  border-radius: 3px;
-  padding: 10px;
-`;
