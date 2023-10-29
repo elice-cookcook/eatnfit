46fa9816801cf127ede537c7760592c1c48cc2a8
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-10-29 16:15:15 +0900

    feat: app.tsx, pages/indx.ts exercise route setting

--- eatnfit-FE/src/App.tsx
+++ eatnfit-FE/src/App.tsx
@@ -1,9 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./pages";
+import {
+  ExerciseRecordPage,
+  AddExercisePage,
+  SearchExercisePage,
+} from "./pages/ExerciseRecordPage";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/exerciserecord" element={<ExerciseRecordPage />} />
+        <Route path="/exerciserecord/add" element={<AddExercisePage />} />
+        <Route path="/exerciserecord/search" element={<SearchExercisePage />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
