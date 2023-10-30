bdd5b94c3da4ecf461a58e07c3eb77f63b96c1b3
최정윤 <ej921004@naver.com>
2023-10-30 10:15:05 +0900

    Chore : move message to AuthInput

--- eatnfit-FE/src/pages/SignUpPage/styles.ts
+++ eatnfit-FE/src/pages/SignUpPage/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  height: 90vh;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -16,20 +16,4 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
 `;
-
-const FeildWrapper = styled.div<{ $message?: boolean }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: ${({ $message }) => ($message ? 0 : "21px")};
-`;
-
-const Message = styled.p`
-  font-weight: bold;
-  font-size: 12px;
-  color: tomato;
-  height: 15px;
-  margin: 3px 0;
-`;
-
-export { Wrapper, Title, Form, FeildWrapper, Message };
+export { Wrapper, Title, Form };
