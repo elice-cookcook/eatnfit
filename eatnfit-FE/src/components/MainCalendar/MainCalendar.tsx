9ebb4de956ba63d36a04105dc3f2218c1ceacdc5
최정윤 <ej921004@naver.com>
2023-10-31 03:11:54 +0900

    Feat : add react-calendar library at MainCalendar component

--- eatnfit-FE/src/components/MainCalendar/MainCalendar.tsx
+++ eatnfit-FE/src/components/MainCalendar/MainCalendar.tsx
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Calendar from "react-calendar";
 import { CalendarWrapper } from "./styles";
+import moment from "moment";
 
 const MainCalendar = () => {
-  return <CalendarWrapper></CalendarWrapper>;
+  const [activeDay, setActiveDay] = useState(new Date());
+  const onChangeToday = (newDate: any) => {
+    setActiveDay(newDate);
+    console.log(activeDay);
+  };
+  return (
+    <CalendarWrapper>
+      <Calendar
+        onChange={onChangeToday}
+        value={activeDay}
+        formatDay={(locale, date) => moment(date).format("DD")}
+      />
+    </CalendarWrapper>
+  );
 };
 
 export default MainCalendar;
