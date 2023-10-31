ffb7191a406a1b2a2d7e36ae865435334cd72eb5
최정윤 <ej921004@naver.com>
2023-10-31 19:40:27 +0900

    Fix : remove duplicated codes

--- eatnfit-FE/src/App.tsx
+++ eatnfit-FE/src/App.tsx
@@ -27,9 +27,6 @@ function App() {
         <Route path="/exerciserecord" element={<ExerciseRecordPage />} />
         <Route path="/exerciserecord/add" element={<AddExercisePage />} />
         <Route path="/exerciserecord/search" element={<SearchExercisePage />} />
-        <Route path="/foodrecord" element={<FoodRecordPage />} />
-        <Route path="/foodrecord/add" element={<AddFoodPage />} />
-        <Route path="/foodrecord/search" element={<SearchFoodPage />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/main" element={<MainFoodPage />} />
         <Route path="/main/exercise" element={<MainExercisePage />} />
