62ff4b935e6a4abf284e4099b42ad8093df3bca2
최정윤 <ej921004@naver.com>
2023-11-02 00:44:18 +0900

    Design : modify design MainRadioButton

--- eatnfit-FE/src/components/MainRadioButton/MainRadioButton.tsx
+++ eatnfit-FE/src/components/MainRadioButton/MainRadioButton.tsx
@@ -1,5 +1,6 @@
 import { Radio } from "antd";
 import { useNavigate } from "react-router-dom";
+import { RadioWrapper } from "./styles";
 
 type MainRadioButtonType = {
   defaultValue: string;
@@ -7,31 +8,33 @@ type MainRadioButtonType = {
 export default function MainRadioButton({ defaultValue }: MainRadioButtonType) {
   const navigate = useNavigate();
   return (
-    <Radio.Group defaultValue={defaultValue} buttonStyle="solid">
-      <Radio.Button
-        value="food"
-        onClick={() => {
-          navigate("/main");
-        }}
-      >
-        식단
-      </Radio.Button>
-      <Radio.Button
-        value="exercise"
-        onClick={() => {
-          navigate("/main/exercise");
-        }}
-      >
-        운동
-      </Radio.Button>
-      <Radio.Button
-        value="plan"
-        onClick={() => {
-          navigate("/main/plan");
-        }}
-      >
-        계획
-      </Radio.Button>
-    </Radio.Group>
+    <RadioWrapper>
+      <Radio.Group defaultValue={defaultValue} buttonStyle="solid">
+        <Radio.Button
+          value="food"
+          onClick={() => {
+            navigate("/main");
+          }}
+        >
+          식단
+        </Radio.Button>
+        <Radio.Button
+          value="exercise"
+          onClick={() => {
+            navigate("/main/exercise");
+          }}
+        >
+          운동
+        </Radio.Button>
+        <Radio.Button
+          value="plan"
+          onClick={() => {
+            navigate("/main/plan");
+          }}
+        >
+          계획
+        </Radio.Button>
+      </Radio.Group>
+    </RadioWrapper>
   );
 }
--- eatnfit-FE/src/components/MainRadioButton/styles.ts
+++ eatnfit-FE/src/components/MainRadioButton/styles.ts
@@ -1,8 +1,21 @@
-import { Radio } from "antd";
 import styled from "styled-components";
 
-export const StyledRadio = styled(Radio.Button)`
-.ant-radio-button-wrapper-checked {
-    background: red !important;
-}
+export const RadioWrapper = styled.div`
+  width: 100%;
+
+  .ant-radio-group {
+    display: flex !important;
+  }
+  .ant-radio-button-wrapper {
+    flex-grow: 1;
+    text-align: center;
+    border-radius: 0;
+  }
+
+  .ant-radio-button-wrapper-checked {
+    flex-grow: 1;
+    text-align: center;
+    background-color: #00a9ff !important;
+    border: none;
+  }
 `;
