dff0170fc5d5aa93e6efcc28de752d0a20b68951
최정윤 <ej921004@naver.com>
2023-11-02 01:57:02 +0900

    Design : modify design DashBoard

--- eatnfit-FE/src/components/Dashboard/styles.ts
+++ eatnfit-FE/src/components/Dashboard/styles.ts
@@ -1,22 +1,28 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    gap: 10px;
-    border: 2px solid #89cff3;
-    padding: 20px;
-    border-radius: 4px;
-    font-size: 12px;
-    margin: 10px 0;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  border: 2px solid #89cff3;
+  padding: 20px;
+  border-radius: 4px;
+  font-size: 12px;
+  margin: 10px auto;
 `;
 export const Contents = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-basis: 120px;
+  display: flex;
+  flex-direction: column;
+  flex-basis: 120px;
+  text-align: center;
 `;
 export const Label = styled.div`
-    color: ${(props) => props.color? props.color: "#00a9ff"};
-    font-weight: bold;
+  color: ${(props) => (props.color ? props.color : "#00a9ff")};
+  font-weight: bold;
+  margin: 3px 0;
+  font-size: 0.8rem;
+`;
+export const Span = styled.span`
+  margin: 3px 0;
+  font-size: 0.7rem;
 `;
