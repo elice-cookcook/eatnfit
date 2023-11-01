a0e3f213371326fd3ba74749ae63a2412d0558f1
최정윤 <ej921004@naver.com>
2023-11-02 02:30:42 +0900

    Feat : add FooterModal component

---
+++ eatnfit-FE/src/components/FooterModal/FooterModal.tsx
@@ -0,0 +1,13 @@
+import { ModalWrapper } from "./styles";
+
+const FooterModal = () => {
+  return (
+    <ModalWrapper>
+      <p>식단</p>
+      <p>운동</p>
+      <p>계획</p>
+    </ModalWrapper>
+  );
+};
+
+export default FooterModal;
---
+++ eatnfit-FE/src/components/FooterModal/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ModalWrapper = styled.div`
+  text-align: center;
+`;
+
+export { ModalWrapper };
