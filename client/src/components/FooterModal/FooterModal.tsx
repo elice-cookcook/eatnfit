60a495ebd81c0610bac4ed087f281f7f171ae6c0
최정윤 <ej921004@naver.com>
2023-11-03 02:36:37 +0900

    Feat : link to foodRecord, exerciseRecord

--- client/src/components/FooterModal/FooterModal.tsx
+++ client/src/components/FooterModal/FooterModal.tsx
@@ -1,18 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { MenuItem, ModalWrapper } from "./styles";
 import { TbApple, TbRun, TbCheckupList } from "react-icons/tb";
 
 const FooterModal = () => {
+  const navigate = useNavigate();
+
   return (
     <ModalWrapper>
-      <MenuItem>
+      <MenuItem onClick={() => navigate("/foodrecord")}>
         <TbApple size={"23px"} />
         <span>식단 추가</span>
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navigate("/exerciserecord")}>
         <TbRun size={"23px"} />
         <span>운동 추가</span>
       </MenuItem>
-      <MenuItem>
+      <MenuItem onClick={() => navigate("/main")}>
         <TbCheckupList size={"23px"} />
         <span>계획 추가</span>
       </MenuItem>
