21922a6410318df94cb69077a7ecba1218993c25
BoyoungYun <index_1234@naver.com>
2023-11-02 21:49:41 +0900

    feat: Integrate FE and BE

---
+++ client/src/hooks/getAllMeal.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { Meal } from '../types';
+
+const getAllMeal = async (date: string): Promise<Meal> => {
+	const response = await axios.get(
+		`/api/v1/meals/${date}`
+	);
+	return response.data;
+};
+export function useGetAllMeal(date: string) {
+	return useQuery("get-all-meal", () => getAllMeal(date));
+}
