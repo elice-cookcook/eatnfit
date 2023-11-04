ac93fcd8898e8dd04bed498eb017042214e34b19
최정윤 <ej921004@naver.com>
2023-11-05 07:47:34 +0900

    Feat : add setSelectedMenu redux

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
+import menuReducer from "./menuReducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  menu: menuReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
