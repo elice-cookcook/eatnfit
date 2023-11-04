4c45c6219f929d335e3860d4f5a0dff0b78b1c73
최정윤 <ej921004@naver.com>
2023-11-05 03:09:26 +0900

    Feat : add EditWeightForm component

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,29 +1,28 @@
 import { Divider } from "antd";
-import React, { useState } from "react";
-import { Container, Contents, InputWrapper, Label, Span } from "./styles";
-import { TbEdit } from "react-icons/tb";
+import React, { ChangeEvent } from "react";
+import { Container, Contents, Label, Span } from "./styles";
+import { EditWeightForm } from "..";
 
 type DashboardType = {
   title: string[];
   description: string[];
   width: number;
   color?: string[];
-  value1?: number; // 현재몸무게
-  value2?: number; //목표몸무게
-  onChange1?: React.Dispatch<React.SetStateAction<any>>; // 현재 몸무게 변경
-  onChange2?: React.Dispatch<React.SetStateAction<any>>; // 목표 몸무게 변경
+  weight1?: number; // 현재몸무게
+  weight2?: number; //목표몸무게
+  onChange1?: (e: ChangeEvent<HTMLInputElement>) => void; // 현재 몸무게 변경
+  onChange2?: (e: ChangeEvent<HTMLInputElement>) => void; // 목표 몸무게 변경
 };
 export default function DashBoard({
   title,
   description,
   width,
   color,
-  value1,
-  value2,
+  weight1,
+  weight2,
   onChange1,
   onChange2,
 }: DashboardType) {
-  const [edit, setEdit] = useState(false);
   return (
     <>
       <Container style={{ width: `${width}%` }}>
@@ -37,36 +36,16 @@ export default function DashBoard({
                   <Label>{item}</Label>
                 )}
                 <Span>
-                  {value1 && value2 && idx === 0 && edit ? (
-                    <InputWrapper>
-                      <input
-                        value={value1}
-                        onChange={onChange1}
-                        type="number"
-                      ></input>
-                      <span>kg /</span>
-                      <input
-                        value={value2}
-                        onChange={onChange2}
-                        type="number"
-                      ></input>
-                      <span>kg</span>
-                    </InputWrapper>
+                  {weight1 && weight2 && onChange1 && onChange2 && idx === 0 ? (
+                    <EditWeightForm
+                      weight1={weight1}
+                      weight2={weight2}
+                      onChange1={onChange1}
+                      onChange2={onChange2}
+                    />
                   ) : (
                     description[idx]
                   )}
-                  {value1 &&
-                    value2 &&
-                    idx === 0 &&
-                    (edit ? (
-                      <button onClick={() => setEdit(!edit)}>완료</button>
-                    ) : (
-                      <TbEdit
-                        color="navy"
-                        size={"0.8rem"}
-                        onClick={() => setEdit(!edit)}
-                      />
-                    ))}
                 </Span>
               </Contents>
               {idx !== 2 && (
--- client/src/components/Dashboard/styles.ts
+++ client/src/components/Dashboard/styles.ts
@@ -37,15 +37,3 @@ export const Span = styled.span`
     cursor: pointer;
   }
 `;
-
-export const InputWrapper = styled.div`
-  input {
-    width: 25px;
-    font-size: 0.6rem;
-  }
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  } // number type input 옆 화살표(증감) 버튼 삭제
-`;
