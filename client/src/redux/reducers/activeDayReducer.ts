7dfc457d2221e91a152520d3d3bab13bd9bbb39b
최정윤 <ej921004@naver.com>
2023-11-07 02:53:08 +0900

    Feat : add activeDay to redux

---
+++ client/src/redux/reducers/activeDayReducer.ts
@@ -0,0 +1,25 @@
+import moment from "moment";
+import { SET_ACTIVE_DAY, setActiveDay } from "../actions";
+
+const initialState = {
+  activeDay: parseInt(moment().format("YYYYMMDD")),
+};
+
+type activeDayActionType = ReturnType<typeof setActiveDay>;
+
+const activeDayReducer = (
+  state = initialState,
+  action: activeDayActionType
+) => {
+  switch (action.type) {
+    case SET_ACTIVE_DAY:
+      return {
+        ...state,
+        activeDay: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default activeDayReducer;
