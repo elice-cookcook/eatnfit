962a9b80d0437d07b7b283af15d022d0e28beb0a
BoyoungYun <index_1234@naver.com>
2023-11-08 22:55:07 +0900

    feat: Modify getAllMeal custom hook and display meal data

--- client/src/types/Meal.ts
+++ client/src/types/Meal.ts
@@ -6,8 +6,13 @@ export interface Meal {
     meal_type: number;
     image_url: string;
     total_kcal: number;
-    items: {
+    items: [{
         _id: string;
+        item: string;
         count: number;
-    }
+        kcal: number;
+    }],
+    total_carbohydrate: number,
+    total_fat: number,
+    total_protein: number,
 }
