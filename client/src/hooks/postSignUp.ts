cf91e7c95674cb610a36a2b3fe260693e3b46886
최정윤 <ej921004@naver.com>
2023-11-10 19:31:31 +0900

    Feat : add signup hooks

---
+++ client/src/hooks/postSignUp.ts
@@ -0,0 +1,62 @@
+import axios, { AxiosError } from "axios";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { User } from "../types";
+
+interface ApiResponse {
+  message: string;
+  data: User;
+}
+
+const postLogin = async (
+  email: string,
+  password: string,
+  name: string,
+  height: number,
+  weight: number,
+  targetWeight: number
+): Promise<ApiResponse> => {
+  const response = await axios.post(`/api/v1/users/signup`, {
+    email,
+    password,
+    name,
+    height,
+    weight,
+    targetWeight,
+  });
+  return response.data;
+};
+
+export function usePostLogin(
+  email: string,
+  password: string,
+  name: string,
+  height: number,
+  weight: number,
+  targetWeight: number
+) {
+  const navigate = useNavigate();
+
+  const isErrorData = (data: any): data is { message: string } => {
+    return data && typeof data === "object" && "message" in data;
+  };
+
+  return useMutation(
+    () => postLogin(email, password, name, weight, height, targetWeight),
+    {
+      onSuccess: () => {
+        alert(
+          `환영합니다. 회원가입에 성공했습니다.\n로그인페이지로 이동합니다.`
+        );
+        navigate("/login");
+      },
+      onError: (error: AxiosError) => {
+        if (error.response && isErrorData(error.response.data)) {
+          alert(error.response.data.message);
+        } else {
+          alert("An unknown error occurred");
+        }
+      },
+    }
+  );
+}
