de50ce9ef17b35e5eab73bb20f7b523f49a245ed
최정윤 <ej921004@naver.com>
2023-11-10 19:31:43 +0900

    Feat : add signup function

--- client/src/pages/SignUpPage/SignUpPage.tsx
+++ client/src/pages/SignUpPage/SignUpPage.tsx
@@ -22,7 +22,7 @@ export default function SignUpPage() {
   const [name, setName] = useState<string>("");
   const [height, setHeight] = useState<number>();
   const [weight, setWeight] = useState<number>();
-  const [goalWeight, setGoalWeight] = useState<number>();
+  const [targetWeight, setTargetWeight] = useState<number>();
 
   const [emailInputStatus, setEmailInputStatus] = useState(InputStatus.NORMAL);
   const [passwordInputStatus, setPasswordInputStatus] = useState(
@@ -38,7 +38,7 @@ export default function SignUpPage() {
   const [weightInputStatus, setWeightInputStatus] = useState(
     InputStatus.NORMAL
   );
-  const [goalWeightInputStatus, setGoalWeightInputStatus] = useState(
+  const [targetWeightInputStatus, setTargetWeightInputStatus] = useState(
     InputStatus.NORMAL
   );
 
@@ -51,12 +51,19 @@ export default function SignUpPage() {
           status={nameInputStatus}
           onChange={(e) => {
             setName(e.target.value);
+            setNameInputStatus(
+              e.target.value.length >= 2
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
           }}
           value={name}
           placeholder="이름"
           message={
             nameInputStatus === InputStatus.ERROR
-              ? "이름을 정확히 입력하세요"
+              ? "이름은 두글자 이상이어야 합니다."
+              : nameInputStatus === InputStatus.SUCCESS
+              ? "사용가능한 이름입니다."
               : ""
           }
         />
@@ -66,10 +73,19 @@ export default function SignUpPage() {
           type="email"
           value={email}
           placeholder="아이디(이메일)"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setEmailInputStatus(
+              e.target.value.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]")
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
+          }}
           message={
             emailInputStatus === InputStatus.ERROR
               ? "아이디는 이메일 형식으로 입력해주세요."
+              : emailInputStatus === InputStatus.SUCCESS
+              ? "사용가능한 아이디입니다."
               : ""
           }
         />
@@ -79,11 +95,20 @@ export default function SignUpPage() {
           type="password"
           value={password}
           placeholder="비밀번호"
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setPasswordInputStatus(
+              e.target.value.length >= 4
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
+          }}
           isPassword
           message={
             passwordInputStatus === InputStatus.ERROR
-              ? "비밀번호는 8자 이상이어야 합니다. "
+              ? "비밀번호는 4글자 이상이어야 합니다."
+              : passwordInputStatus === InputStatus.SUCCESS
+              ? "사용가능한 비밀번호입니다."
               : ""
           }
         />
@@ -93,11 +118,20 @@ export default function SignUpPage() {
           type="password"
           value={confirmPassword}
           placeholder="비밀번호 확인"
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+            setConfirmPasswordInputStatus(
+              e.target.value === password
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
+          }}
           isPassword
           message={
             confirmPasswordInputStatus === InputStatus.ERROR
               ? "비밀번호가 일치하지 않습니다."
+              : confirmPasswordInputStatus === InputStatus.SUCCESS
+              ? "비밀번호가 일치합니다."
               : ""
           }
         />
@@ -105,26 +139,50 @@ export default function SignUpPage() {
           status={heightInputStatus}
           icon={<TbMan size={"20px"} />}
           type="number"
-          value={height}
+          value={height || ""}
           placeholder="신장(키)"
-          onChange={(e) => setHeight(parseInt(e.target.value))}
+          onChange={(e) => {
+            setHeight(parseFloat(e.target.value));
+            setHeightInputStatus(
+              parseFloat(e.target.value) >= 100 &&
+                parseFloat(e.target.value) <= 300
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
+          }}
           unit={"cm"}
         />
         <AuthInput
           status={weightInputStatus}
           icon={<TbHeart size={"20px"} />}
           type="number"
-          value={weight}
+          value={weight || ""}
           placeholder="체중(몸무게)"
-          onChange={(e) => setWeight(parseInt(e.target.value))}
+          onChange={(e) => {
+            setWeight(parseFloat(e.target.value));
+            setWeightInputStatus(
+              parseFloat(e.target.value) >= 40 &&
+                parseFloat(e.target.value) <= 200
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
+          }}
           unit={"kg"}
         />
         <AuthInput
-          status={goalWeightInputStatus}
+          status={targetWeightInputStatus}
           type="number"
-          value={goalWeight}
+          value={targetWeight || ""}
           placeholder="목표 체중(몸무게)"
-          onChange={(e) => setGoalWeight(parseInt(e.target.value))}
+          onChange={(e) => {
+            setTargetWeight(parseFloat(e.target.value));
+            setTargetWeightInputStatus(
+              parseFloat(e.target.value) >= 40 &&
+                parseFloat(e.target.value) <= 200
+                ? InputStatus.SUCCESS
+                : InputStatus.ERROR
+            );
+          }}
           icon={<TbHeartFilled size={"20px"} />}
           unit={"kg"}
         />
