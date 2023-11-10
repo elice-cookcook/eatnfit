029932fa814b9ee0a278c0c7ab905dd8c0a766b2
최정윤 <ej921004@naver.com>
2023-11-11 04:27:40 +0900

    Feat : add props at SubmitBtn

--- client/src/components/SubmitBtn/SubmitBtn.tsx
+++ client/src/components/SubmitBtn/SubmitBtn.tsx
@@ -2,9 +2,19 @@ import { WrappedSubmitBtn } from "./styltes";
 
 interface SubmitBtnProps {
   onSubmit: () => void;
+  text?: string;
+  color?: string;
 }
-const SubmitBtn = ({ onSubmit }: SubmitBtnProps) => {
-  return <WrappedSubmitBtn onClick={onSubmit}>등록</WrappedSubmitBtn>;
+const SubmitBtn = ({
+  onSubmit,
+  text = "등록",
+  color = "#89cff3",
+}: SubmitBtnProps) => {
+  return (
+    <WrappedSubmitBtn onClick={onSubmit} style={{ backgroundColor: color }}>
+      {text}
+    </WrappedSubmitBtn>
+  );
 };
 
 export default SubmitBtn;
