bc4e5cd18003b67d52a9879a634060244a11d41b
최정윤 <ej921004@naver.com>
2023-12-29 07:54:09 +0900

    Feat : split routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  LandingPage,
-  SignUpPage,
-  ExerciseRecordPage,
-  AddExercisePage,
-  SearchExercisePage,
-  FoodRecordPage,
-  AddFoodPage,
-  SearchFoodPage,
-  LoginPage,
-  ExerciseDetailPage,
-  MainPage,
-  FoodDetailPage,
-} from "./pages";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { ConfigProvider } from "antd";
+import { ROUTE_ARR } from "./routes/Route";
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/foodrecord" element={<FoodRecordPage />} />
-          <Route path="/foodrecord/add" element={<AddFoodPage />} />
-          <Route path="/foodrecord/search" element={<SearchFoodPage />} />
-          <Route path="/food/:date/:idx" element={<FoodDetailPage />} />
-          <Route path="/signUp" element={<SignUpPage />} />
-          <Route path="/exercise/:date/:idx" element={<ExerciseDetailPage />} />
-          <Route path="/exerciserecord" element={<ExerciseRecordPage />} />
-          <Route path="/exerciserecord/add" element={<AddExercisePage />} />
-          <Route
-            path="/exerciserecord/search"
-            element={<SearchExercisePage />}
-          />
-          <Route path="/main" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
+      <ConfigProvider>
+        <BrowserRouter>
+          <Routes>
+            {ROUTE_ARR.map((el) => (
+              <Route path={el.path} key={el.path} element={el.element} />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
     </Provider>
   );
 }
