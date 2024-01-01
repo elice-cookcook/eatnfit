a52145cfea4037142ea0bcab191d8619b39569d1
BoyoungYun <index_1234@naver.com>
2024-01-01 21:14:36 +0900

    feat: Add message when success and error

--- client/src/hooks/postMeal.ts
+++ client/src/hooks/postMeal.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useMutation, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
+import { message } from "antd";
 import { Meal } from '../types';
 
 const postMeal = async (date: string, meal: Meal): Promise<Meal> => {
@@ -17,7 +18,12 @@ export function usePostMeal(date: string) {
 
     return useMutation((meal: Meal) => postMeal(date, meal), {
         onSuccess: () => {
-        navigate('/main');
-        queryClient.invalidateQueries("get-all-meal");
-    }});
+            message.success("식단 추가에 성공하였습니다.")
+            navigate('/main');
+            queryClient.invalidateQueries("get-all-meal");
+        },
+        onError: () => {
+            message.error("식단 추가에 실패하였습니다.");
+        }
+    });
 }
