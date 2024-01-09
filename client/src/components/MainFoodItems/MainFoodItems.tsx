27a7f3187e8ea864b5d47e78bc958a9682ea6aba
[SW6] 이선우 <sunwoo40101@gmail.com>
2024-01-09 12:01:20 +0900

    Feat: popConfirm from antd

--- client/src/components/MainFoodItems/MainFoodItems.tsx
+++ client/src/components/MainFoodItems/MainFoodItems.tsx
@@ -16,7 +16,8 @@ import { useNavigate } from "react-router-dom";
 import { RootState } from "../../redux";
 import { Meal } from "../../types";
 import { useDeleteMeal } from "../../hooks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Popconfirm } from "antd";
 
 type MainFoodItemsType = {
   items: Meal[];
@@ -31,21 +32,25 @@ export default function MainFoodItems({ items, totalKcal }: MainFoodItemsType) {
 
   const mealType = ["아침", "아점", "점심", "간식", "점저", "저녁", "야식"];
   const [mealId, setMealId] = useState<string>("");
+  const [isNavMoved, setNavMoved] = useState<boolean>(false);
 
   const deleteMeal = useDeleteMeal(mealId);
 
-  const handleDeleteMeal = (idx: number) => {
+  const handleOpenChange = (idx: number) => {
     const mealToDeleteId = items[idx]._id;
     setMealId(mealToDeleteId);
-    if (confirm("식단 기록을 삭제하시겠습니까?")) {
-      deleteMeal.mutate();
-    }
+    setNavMoved(true);
+  };
+
+  const handleDeleteMeal = () => {
+    if (mealId) deleteMeal.mutate();
   };
 
   return items.map((item, idx) => (
     <Container
       key={idx}
       onClick={() =>
+        !isNavMoved &&
         nav(`${ROUTE.FOOD_DETAIL_PAGE.link}/${activeDay}/${idx}`, {
           state: { isEdit: false },
         })
@@ -58,12 +63,20 @@ export default function MainFoodItems({ items, totalKcal }: MainFoodItemsType) {
           <Time>{`${String(item.time).slice(0, 2)}:${String(item.time).slice(
             2
           )}`}</Time>
-          <DeleteTwoTone
-            onClick={(e) => {
-              e.stopPropagation();
-              handleDeleteMeal(idx);
-            }}
-          />
+          <Popconfirm
+            title="식단기록 삭제"
+            description="식단 기록을 삭제하시겠습니까?"
+            onConfirm={handleDeleteMeal}
+            okText="네"
+            cancelText="아니요"
+          >
+            <DeleteTwoTone
+              onClick={(e) => {
+                e.stopPropagation();
+                handleOpenChange(idx);
+              }}
+            />
+          </Popconfirm>
         </TitleBlock>
         <StyledList>
           {item.items.map((list, idx) => (
