c57ca7eb946996a8133d2f9848fa0baa9f6dc867
최정윤 <ej921004@naver.com>
2024-01-15 17:23:19 +0900

    Feat : patch userInfo at UserProfile of SettingPage

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -1,35 +1,101 @@
-import { Container, ContentItem } from "./styles";
+import { Container, ContentItem, ContentValue, EditButton } from "./styles";
 import { User } from "../../types/User";
+import { useState } from "react";
+import { usePatchUserInfo } from "../../hooks";
 
-const UserProfile = ({ userData }: { userData: User }) => {
+interface UserProfileProps {
+  userData: User;
+}
+
+const UserProfile = ({ userData }: UserProfileProps) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [name, setName] = useState(userData.name);
+  const [height, setHeight] = useState(userData.height);
+  const [weight, setWeight] = useState(userData.weight);
+  const [targetWeight, setTargetWeight] = useState(userData.target_weight);
+
+  const { mutate: patchUserInfo } = usePatchUserInfo(
+    name,
+    height,
+    weight,
+    targetWeight
+  );
+
+  const handlePatchUserInfo = () => {
+    patchUserInfo();
+    setIsEdit(false);
+  };
+
   return (
     <>
-      {userData && (
-        <Container>
-          <ContentItem>
-            <label>이메일</label>
-            <span>{userData.email}</span>
-          </ContentItem>
-          <ContentItem>
-            <label>이름</label>
-            <span>{userData.name}</span>
-          </ContentItem>
-          <ContentItem>
-            <label>키</label>
-            <span>
-              {userData.height}
-              <span className="unit">cm</span>
-            </span>
-          </ContentItem>
-          <ContentItem>
-            <label>현재/목표 몸무게</label>
-            <span>
-              {userData.weight}/{userData.target_weight}
-              <span className="unit">kg</span>
-            </span>
-          </ContentItem>
-        </Container>
+      {isEdit ? (
+        <EditButton onClick={handlePatchUserInfo}>수정완료</EditButton>
+      ) : (
+        <EditButton onClick={() => setIsEdit(true)}>수정하기</EditButton>
       )}
+      <Container>
+        <ContentItem>
+          <label>이메일</label>
+          <ContentValue>
+            <span>{userData.email}</span>
+          </ContentValue>
+        </ContentItem>
+        <ContentItem>
+          <label>이름</label>
+          <ContentValue>
+            {isEdit ? (
+              <input value={name} onChange={(e) => setName(e.target.value)} />
+            ) : (
+              <span>{name}</span>
+            )}
+          </ContentValue>
+        </ContentItem>
+        <ContentItem>
+          <label>키</label>
+          <ContentValue>
+            {isEdit ? (
+              <input
+                type={"number"}
+                value={height}
+                onChange={(e) => setHeight(parseFloat(e.target.value))}
+              />
+            ) : (
+              <span>{height}</span>
+            )}
+            <span className="unit">cm</span>
+          </ContentValue>
+        </ContentItem>
+        <ContentItem>
+          <label>현재몸무게</label>
+          <ContentValue>
+            {isEdit ? (
+              <input
+                type={"number"}
+                value={weight}
+                onChange={(e) => setWeight(parseFloat(e.target.value))}
+              />
+            ) : (
+              <span>{weight}</span>
+            )}
+            <span className="unit">kg</span>
+          </ContentValue>
+        </ContentItem>
+        <ContentItem>
+          <label>목표 몸무게</label>
+          <ContentValue>
+            {isEdit ? (
+              <input
+                type={"number"}
+                value={targetWeight}
+                onChange={(e) => setTargetWeight(parseFloat(e.target.value))}
+              />
+            ) : (
+              <span>{targetWeight}</span>
+            )}
+            <span className="unit">kg</span>
+          </ContentValue>
+        </ContentItem>
+      </Container>
     </>
   );
 };
--- client/src/components/UserProfile/styles.ts
+++ client/src/components/UserProfile/styles.ts
@@ -4,26 +4,50 @@ export const Container = styled.div`
   border: 1px solid #00a9ff;
   border-radius: 10px;
   padding: 10px;
-  margin: 10px 0;
+  margin-bottom: 10px;
+`;
+
+export const EditButton = styled.button`
+  border: none;
+  padding: 5px;
+  background-color: transparent;
+  text-decoration: underline;
+  margin-left: auto;
+  color: #00a9ff;
+  font-size: 16px;
 `;
 
 export const ContentItem = styled.div`
+  height: 20px;
   margin: 10px 5px;
   display: flex;
   text-align: center;
+
   label {
+    width: 50%;
     color: #00a9ff;
     font-weight: bold;
     text-align: center;
     margin: 0 5px;
-    flex: 1;
   }
-  span {
-    color: black;
-    flex: 1;
-    .unit {
-      color: gray;
-      margin: 0 3px;
+`;
+
+export const ContentValue = styled.span`
+  width: 50%;
+
+  .unit {
+    color: gray;
+    margin: 0 3px;
+  }
+
+  input {
+    width: 50%;
+    border: none;
+    border-bottom: 1px solid gray;
+    text-align: center;
+    padding: 5px;
+    &:focus {
+      outline: none;
     }
   }
 `;
